115f0d25b4d5ab92fe54bbb8ada0d9a246fb4f82
Frederick Gnodtke <frederick@gnodtke.net>
2022-10-12 23:49:05 +0200

    Fix config_flow error

--- config_flow.py
+++ config_flow.py
@@ -10,6 +10,7 @@ from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN
 from .oilfox import OilfoxHub
@@ -25,7 +26,7 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
 
 
 async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> None:
-    hub = OilfoxHub()
+    hub = OilfoxHub(async_get_clientsession(hass))
 
     if not await hub.authenticate(data["email"], data["password"]):
         raise InvalidAuth
